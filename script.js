ce2787707e494bbe810d3b3039f490191c3c95fa
kassymovz <kassymovz@bk.ru>
2024-10-20 15:03:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function num1(){
+    document.getElementById("notification").innerHTML = "Куплено 1";
+    document.getElementById("notification").style.fontSize = "29px";
+    document.getElementById("notification").style.fontFamily = "Cambria, Cochin, Georgia, Times, 'Times New Roman";
+}
+function num2(){
+    document.getElementById("notification-2").innerHTML = "Куплено 2";
+    document.getElementById("notification-2").style.fontSize = "29px";
+    document.getElementById("notification").style.fontFamily = "Cambria, Cochin, Georgia, Times, 'Times New Roman";
+
+}
+function num3(){
+    document.getElementById("notification-3").innerHTML = "Куплено 3";
+    document.getElementById("notification-3").style.fontSize = "29px";
+}
+function num4(){
+    document.getElementById("notification").innerHTML = "Куплено 4";
+    docunmet.getElementById("notidication-4").style.fontSize = "29px";
+}
